8f6941f39cc14e4bed3c3d315944dafde451a401
iihll <iihll.dev@gmail.com>
2022-03-24 21:41:43 +0800

    feat: 添加工程列表

---
+++ src/screens/project-list/index.jsx
@@ -0,0 +1,41 @@
+import { List } from "./list";
+import { SearchPanel } from "./search-panel";
+import { useState, useEffect } from "react";
+import { stringify } from "qs";
+import { clearObject } from "utils";
+
+const apiUrl = process.env.REACT_APP_API_URI;
+
+export const ProjectListScreen = () => {
+  const [users, setUsers] = useState([]);
+  const [param, setParam] = useState({
+    name: "",
+    personId: "",
+  });
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    fetch(`${apiUrl}/projects?${stringify(clearObject(param))}`).then(
+      async (response) => {
+        if (response.ok) {
+          setList(await response.json());
+        }
+      }
+    );
+  }, [param]);
+
+  useEffect(() => {
+    fetch(`${apiUrl}/users`).then(async (response) => {
+      if (response.ok) {
+        setUsers(await response.json());
+      }
+    });
+  }, []);
+
+  return (
+    <div>
+      <SearchPanel param={param} setParam={setParam} users={users} />
+      <List list={list} users={users} />
+    </div>
+  );
+};
