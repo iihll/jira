8e207eb77e016958c016746fefcd117cbbc9760e
iihll <iihll.dev@gmail.com>
2022-04-01 21:28:53 +0800

    feat: 添加登录状态维持

--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -1,42 +1,31 @@
-import { List } from "./list";
-import { SearchPanel } from "./search-panel";
-import { useState, useEffect } from "react";
-import { stringify } from "qs";
-import { clearObject, useDebounce, useMount } from "utils";
-
-const apiUrl = process.env.REACT_APP_API_URL;
+import { List } from './list'
+import { SearchPanel } from './search-panel'
+import { useState, useEffect } from 'react'
+import { clearObject, useDebounce, useMount } from 'utils'
+import { useHttp } from 'utils/http'
 
 export const ProjectListScreen = () => {
-  const [users, setUsers] = useState([]);
+  const [users, setUsers] = useState([])
   const [param, setParam] = useState({
-    name: "",
-    personId: "",
-  });
-  const debouncedParam = useDebounce(param, 200);
-  const [list, setList] = useState([]);
+    name: '',
+    personId: '',
+  })
+  const debouncedParam = useDebounce(param, 200)
+  const [list, setList] = useState([])
+  const client = useHttp()
 
   useEffect(() => {
-    fetch(`${apiUrl}/projects?${stringify(clearObject(debouncedParam))}`).then(
-      async (response) => {
-        if (response.ok) {
-          setList(await response.json());
-        }
-      }
-    );
-  }, [debouncedParam]);
+    client('projects', { data: clearObject(debouncedParam) }).then(setList)
+  }, [debouncedParam])
 
   useMount(() => {
-    fetch(`${apiUrl}/users`).then(async (response) => {
-      if (response.ok) {
-        setUsers(await response.json());
-      }
-    });
-  });
+    client('users').then(setUsers)
+  })
 
   return (
     <div>
       <SearchPanel param={param} setParam={setParam} users={users} />
       <List list={list} users={users} />
     </div>
-  );
-};
+  )
+}
